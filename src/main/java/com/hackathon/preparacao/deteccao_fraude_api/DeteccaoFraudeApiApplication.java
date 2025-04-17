72e1824ecfe7005f3c654829a6f6ee16b5bb1e62
Bruno Henrique Ramos <bruno.hr.gay@gmail.com>
2025-04-17 18:49:01 -0300

    :tada: criação do projeto

---
+++ src/main/java/com/hackathon/preparacao/deteccao_fraude_api/DeteccaoFraudeApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.preparacao.deteccao_fraude_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeteccaoFraudeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeteccaoFraudeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/preparacao/deteccao_fraude_api/DeteccaoFraudeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.preparacao.deteccao_fraude_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeteccaoFraudeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
