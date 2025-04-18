ce293d7981732e8843c3c08a58eef96d55bc12d4
Bruno Henrique Ramos <bruno.hr.gay@gmail.com>
2025-04-17 23:00:32 -0300

    :reclycle: fix(Transacao) coloca getter, setter e construtor não colocado anteriormente

--- src/main/java/com/hackathon/preparacao/deteccao_fraude_api/domain/Transacao.java
+++ src/main/java/com/hackathon/preparacao/deteccao_fraude_api/domain/Transacao.java
@@ -1,9 +1,16 @@
 package com.hackathon.preparacao.deteccao_fraude_api.domain;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transacao {
 
     @Id
