d8a939516a2e2d03460bf51f5a7c347fb7e17658
tulio <tulio.jacinto32@gmail.com>
2025-04-19 16:39:53 -0300

    feat(validarQuantidadeTransferencias) algumas organizações para o clean code e implementação do validarGrandeGasto

--- src/main/java/com/hackathon/preparacao/deteccao_fraude_api/utils/LogicaValidacoes.java
+++ src/main/java/com/hackathon/preparacao/deteccao_fraude_api/utils/LogicaValidacoes.java
@@ -2,9 +2,11 @@ package com.hackathon.preparacao.deteccao_fraude_api.utils;
 
 import com.hackathon.preparacao.deteccao_fraude_api.domain.Transacao;
 import com.hackathon.preparacao.deteccao_fraude_api.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,21 +14,24 @@ import java.util.stream.Collectors;
 @Component
 public class LogicaValidacoes {
 
-    TransacaoService transacaoService;
+    @Autowired
+    private TransacaoService transacaoService;
 
-    public boolean validarMesmaTitularidade(Transacao transacao) {
-        return true ? transacao.getUserId().equals(transacao.getDestino().getIdDestino()) : false;
+    private List<Transacao> getListaTransacao(Transacao transacao) {
+        return transacaoService.buscarListaTransacoesPorCliente(transacao.getUserId());
     }
 
-    public boolean validarValorMedioGastoDiario(Transacao transacao) {
-        List<Transacao> listaTransacao = transacaoService.buscarListaTransacoesPorCliente(transacao.getUserId());
+    public boolean validarMesmaTitularidade(Transacao transacao) {
+        return transacao.getUserId().equals(transacao.getDestino().getIdDestino());
+    }
 
+
+    public boolean validarValorMedioGastoDiario(Transacao transacao) {
+        List<Transacao> listaTransacao = getListaTransacao(transacao);
         LocalDateTime hoje = LocalDateTime.now();
         LocalDateTime trintaDiasAtras = hoje.minusDays(30);
 
-        List<Transacao> ultimosTrintaDias = listaTransacao.stream()
-                .filter(t -> t.getDataTransacao().isAfter(trintaDiasAtras) && t.getDataTransacao().isBefore(hoje))
-                .collect(Collectors.toList());
+        List<Transacao> ultimosTrintaDias = transacoesPorPeriodo(trintaDiasAtras, hoje, listaTransacao);
 
         double somaTotal = ultimosTrintaDias.stream()
                 .mapToDouble(Transacao::getSaldoTransacao)
@@ -38,27 +43,39 @@ public class LogicaValidacoes {
     }
 
     public boolean validarQuantidadeTransferencias(Transacao transacao) {
-        List<Transacao> listaTransacao = transacaoService.buscarListaTransacoesPorCliente(transacao.getUserId());
+        List<Transacao> listaTransacao = getListaTransacao(transacao);
 
         int qtdLimiteTransferencias = 5;
-        int espacoTempoTransferencias = 5;
+        int tempoEntreTransferencias = 5;
 
         long quantidadeTransacoes = listaTransacao.stream()
-                .filter(t -> t.getDataTransacao().isAfter(t.getDataTransacao().minusMinutes(espacoTempoTransferencias)))
+                .filter(t -> t.getDataTransacao().isAfter(transacao.getDataTransacao().minusMinutes(tempoEntreTransferencias)))
                 .count();
 
-        if (qtdLimiteTransferencias >= quantidadeTransacoes) {
-            return false;
-        } else {
-            return true;
-        }
+        return quantidadeTransacoes > qtdLimiteTransferencias;
 
     }
 
     public boolean validarGrandeGasto(Transacao transacao) {
-        return true;
+        List<Transacao> listaTransacao = getListaTransacao(transacao);
+
+        LocalDateTime hoje = LocalDateTime.now();
+        LocalDateTime trintaDiasAtras = hoje.minusDays(30);
+
+        List<Transacao> ultimosTrintaDias = listaTransacao.stream()
+                .filter(t -> t.getDataTransacao().isAfter(trintaDiasAtras) && t.getDataTransacao().isBefore(hoje))
+                .collect(Collectors.toList());
+
+        double maiorTransacaoTrintaDias = ultimosTrintaDias.stream()
+                .mapToDouble(Transacao::getSaldoTransacao)
+                .max()
+                .orElse(0);
+
+        return transacao.getSaldoTransacao() > maiorTransacaoTrintaDias;
     }
+
     public boolean validarFraudeRecebedor(Transacao transacao) {
+
         return true;
     }
     public boolean validarLocalidadeGeografica(Transacao transacao) {
@@ -72,5 +89,12 @@ public class LogicaValidacoes {
     }
     public boolean validarPadraoTriangulo(Transacao transacao) {
         return true;
+    }
+
+    public List<Transacao> transacoesPorPeriodo(LocalDateTime dataInicio, LocalDateTime dataFim, List<Transacao> listaTransacao ) {
+        List<Transacao> transacoesNoPeriodo = listaTransacao.stream()
+                .filter(t -> t.getDataTransacao().isAfter(dataInicio) && t.getDataTransacao().isBefore(dataFim))
+                .collect(Collectors.toList());
+        return transacoesNoPeriodo;
     }
 }
